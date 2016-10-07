47d1aff80e10526bd176e2c468b2436731670d59
Qawelesizwe Mlilo <qawemlilo@gmail.com>
2016-10-07 07:36:20 +0200

    removed unnecessary packages

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-
+"use strict"
 // load main lib
 module.exports = require('./lib/loader');
--- test/widgets/copyright/index.js
+++ test/widgets/copyright/index.js
@@ -1,10 +1,9 @@
 "use strict";
 
 const config = require('./config.json');
-const when = require('when');
 
 module.exports.config = config;
 
 module.exports.exec = function (App) {
-  return when(null);
+  return Promise.resolve(null);
 };
